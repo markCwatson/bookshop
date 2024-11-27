7f9d606a2f7010c99b6e2be62344d0a15e411115
mark watson <markus.c.watson@gmail.com>
2024-11-27 09:17:15 -0400

    Seed Categories table

--- Bookstore/Data/AppDbContext.cs
+++ Bookstore/Data/AppDbContext.cs
@@ -10,5 +10,15 @@ namespace Bookstore.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Programming", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Databases", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Web Design", DisplayOrder = 3 },
+                new Category { Id = 4, Name = "Networking", DisplayOrder = 4 }
+            );
+        }
     }
 }
