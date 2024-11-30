fe276e7b9076d64e6d94a6e4da240f38aa95eff1
mark watson <markus.c.watson@gmail.com>
2024-11-30 09:32:55 -0400

    Edit and Delete Categories

--- Bookstore/Controllers/CategoryController.cs
+++ Bookstore/Controllers/CategoryController.cs
@@ -37,5 +37,69 @@ namespace Bookstore.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index", "Category");
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category updatedCategory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Edit");
+            }
+
+            _db.Categories.Update(updatedCategory);
+            _db.SaveChanges();
+            return RedirectToAction("Index", "Category");
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? toDelete = _db.Categories.Find(id);
+            if (toDelete == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(toDelete);
+            _db.SaveChanges();
+            return RedirectToAction("Index", "Category");
+        }
     }
 }
